5b5c11a765e02b47a4e52cc44b9aad2f64625ded
prats7 <pratyushvishal04@gmail.com>
2020-12-02 12:58:32 +0530

    Setup socket.io

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ const path = require('path');
 
 const app = express();
 
+const socketio = require('socket.io');
+
 const port = 8000;
 
 const server = http.createServer(app);
 
+const io = socketio(server);
+
 //Set public folder for front-end
 app.use(express.static(path.join(__dirname,'public')));
 
+//Run when user connects
+io.on('connection', socket => {
+    console.log('Web socket Connection...');
+});
 
 server.listen(port, function(err){
     if (err){
